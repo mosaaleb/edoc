257f8272f09cc2c5e0786622df3161bd0ba1083e
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-01-18 01:58:10 +0200

    server side validations component

---
+++ src/components/ValidationError.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ValidationError = ({ isValid, error }) => {
+  const errors = error.map((err) => (
+    <p key={err} className="text-red-600">
+      *
+      {err}
+    </p>
+  ));
+  return <div className="h-6">{isValid ? null : errors}</div>;
+};
+
+ValidationError.defaultProps = {
+  error: []
+};
+
+ValidationError.propTypes = {
+  isValid: PropTypes.bool.isRequired,
+  error: PropTypes.string
+};
+
+export default ValidationError;
