7dedd731fc8c67a141adeb5d2de15d59fbc4a47c
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-04-01 00:19:04 +0200

    review date from now relative

--- src/components/doctors/DoctorReview.js
+++ src/components/doctors/DoctorReview.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 import PropTypes from 'prop-types';
 
 const DoctorReview = ({ review }) => {
@@ -11,7 +12,7 @@ const DoctorReview = ({ review }) => {
   return (
     <div className="shadow-md p-4 my-10">
       <div>
-        <div className="flex items-center pb-4">
+        <div className="flex items-center pb-1">
           <div className="w-12 h-12  rounded-full overflow-hidden">
             <img
               src={reviewerAvatarUrl}
@@ -19,17 +20,19 @@ const DoctorReview = ({ review }) => {
               className="h-full w-full object-cover block"
             />
           </div>
-          <div className="pl-2 text-lg">
-            <span className="text-teal-500 font-bold">{reviewerName}</span>
-            {' '}
-            Said:
+          <div className="pl-2">
+            <div className="text-lg">
+              <span className="text-teal-500 font-bold">{reviewerName}</span>
+              {' '}
+              Said:
+            </div>
+            <div className="text-xs font-bold text-gray-500">{moment(createdAt).fromNow()}</div>
           </div>
         </div>
       </div>
-      <div>
+      <div className="text-gray-700 my-3">
         {text}
       </div>
-      <div>{createdAt}</div>
     </div>
   );
 };
