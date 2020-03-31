3a3ce405c020c41ef1b34d253e1d90a88d7daf82
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-03-31 23:22:24 +0200

    small screen reviews styles

--- src/components/doctors/DoctorReview.js
+++ src/components/doctors/DoctorReview.js
@@ -1,14 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const DoctorReview = ({ review }) => (
-  <div className="shadow-md p-4 my-5">
-    {review.review}
-  </div>
-);
+const DoctorReview = ({ review }) => {
+  const {
+    createdAt,
+    reviewerName,
+    review: text,
+    reviewerAvatarUrl
+  } = review;
+  return (
+    <div className="shadow-md p-4 my-10">
+      <div>
+        <div className="flex items-center pb-4">
+          <div className="w-12 h-12  rounded-full overflow-hidden">
+            <img
+              src={reviewerAvatarUrl}
+              alt={`${reviewerName}'s avatar`}
+              className="h-full w-full object-cover block"
+            />
+          </div>
+          <div className="pl-2 text-lg">
+            <span className="text-teal-500 font-bold">{reviewerName}</span>
+            {' '}
+            Said:
+          </div>
+        </div>
+      </div>
+      <div>
+        {text}
+      </div>
+      <div>{createdAt}</div>
+    </div>
+  );
+};
 
 DoctorReview.propTypes = {
-  review: PropTypes.objectOf({}).isRequired
+  review: PropTypes.objectOf({
+    id: PropTypes.number,
+    review: PropTypes.string,
+    createdAt: PropTypes.string,
+    reviewerName: PropTypes.string,
+    reviewerAvatarUrl: PropTypes.string
+  }).isRequired
 };
 
 export default DoctorReview;
