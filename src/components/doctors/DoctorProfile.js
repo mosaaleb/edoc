2ac616b0b3c4edec11e19e02c04ecbcefb79705a
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-05-17 05:18:26 +0200

    add loading component until async doctor becomes ready

--- src/components/doctors/DoctorProfile.js
+++ src/components/doctors/DoctorProfile.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import axiosInstance from '../../configureAxios';
 import { setIsLoading, resetIsLoading } from '../../actions/loadingActions';
 import { removeCurrentUser } from '../../actions/authActions';
 import { setNotificationMessage } from '../../actions/notificationActions';
 import DoctorReviews from './DoctorReviews';
 import DoctorProfileHeader from './DoctorProfileHeader';
+import Loading from '../Loading';
 
-const DoctorProfile = () => {
+const DoctorProfile = ({ loading, setIsLoading, resetIsLoading }) => {
   const params = useParams();
   const history = useHistory();
   const [doctor, setDoctor] = useState({});
@@ -27,16 +30,41 @@ const DoctorProfile = () => {
       .then(() => {
         resetIsLoading();
       });
-  }, [params, history]);
+  }, [params, history, setIsLoading, resetIsLoading]);
 
   return (
-    <div className="w-full -mt-10">
-      <DoctorProfileHeader doctor={doctor} />
-      <div className="px-4 py-10 font-montserrat">
-        <DoctorReviews reviews={doctor.reviews} />
-      </div>
+    <div>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="w-full -mt-10">
+          <DoctorProfileHeader doctor={doctor} />
+          <div className="px-4 py-10 font-montserrat">
+            <DoctorReviews reviews={doctor.reviews} />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
 
-export default DoctorProfile;
+DoctorProfile.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  setIsLoading: PropTypes.func.isRequired,
+  resetIsLoading: PropTypes.func.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  loading: state.loading
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setIsLoading: () => {
+    dispatch(setIsLoading());
+  },
+  resetIsLoading: () => {
+    dispatch(resetIsLoading());
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(DoctorProfile);
